d05903268855a0b9575de8587e29047885691840
Gongqi Huang <gongqih@hotmail.com>
2023-03-30 22:49:21 -0400

    a simple tracer

--- checkside.py
+++ checkside.py
@@ -2,23 +2,26 @@ import angr
 
 def track_instructions(state):
     addr = state.inspect.instruction
-    if addr is not None:
-        print('... at ', hex(addr), ' ', )
-        state.block(addr, num_inst=1).pp()
+    state.block(addr, num_inst=1).pp()
 
-def foo(state):
-    print('', state.inspect.simprocedure_name)
-
-def check(path):
+def simple_tracer(path):
     p = angr.Project(path, auto_load_libs=False)
     s = p.factory.entry_state()
-    s.inspect.b('instruction', when=angr.BP_AFTER, action=track_instructions)
+    s.inspect.b('instruction', when=angr.BP_BEFORE, action=track_instructions)
     sm = p.factory.simulation_manager(s)
     sm.run()
-    return sm.deadended
+
+    traces = {}
+    for d in sm.deadended:
+        traces[d] = []
+        for a in d.history.bbl_addrs:
+            traces[d].append(d.block(a, num_inst=1))
+            d.block(a, num_inst=1).pp()
+            print(d.block(a, num_inst=1).disassembly)
+
+    return traces
 
 if __name__ == '__main__':
     path = 'examples/branch'
-    ret = check(path)
-    print(ret)
+    ret = simple_tracer(path)
 
